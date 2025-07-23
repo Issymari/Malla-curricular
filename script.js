140aab32edde676c979bb359cbe52388dbd4e04e
Issymari <icebergmp060@gmail.com>
2025-07-23 03:20:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -37,6 +37,97 @@ const ciclos = [
       "Investigación Cuantitativa",
       "Taller: Desarrollo de Competencias Personales III"
     ]
+  },
+  {
+    nombre: "Ciclo 4",
+    cursos: [
+      "Investigación Cualitativa",
+      "Evaluación Psicológica II",
+      "Psicopatología",
+      "Psicología Social",
+      "Psicología de la Percepción",
+      "Psicología Educacional",
+      "Taller de Herramientas Digitales",
+      "Taller: Desarrollo de Competencias Personales IV"
+    ]
+  },
+  {
+    nombre: "Ciclo 5",
+    cursos: [
+      "Psicoterapia Cognitivo Conductual",
+      "Psicología Comunitaria",
+      "Neuropsicología",
+      "Psicología de la Personalidad II",
+      "Psicología Clínica I",
+      "Taller de Entrevista Psicológica",
+      "Práctica Pre Profesional I",
+      "Taller: Desarrollo de Competencias Personales V"
+    ]
+  },
+  {
+    nombre: "Ciclo 6",
+    cursos: [
+      "Psicología Clínica II",
+      "Psicología Organizacional",
+      "Psicología de la Salud",
+      "Psicología Jurídica",
+      "Taller de Evaluación Psicológica",
+      "Intervención Psicológica en Crisis",
+      "Práctica Pre Profesional II",
+      "Taller: Desarrollo de Competencias Personales VI"
+    ]
+  },
+  {
+    nombre: "Ciclo 7",
+    cursos: [
+      "Taller de Psicoterapia",
+      "Taller de Casos Clínicos",
+      "Psicología del Trabajo",
+      "Intervención Psicológica Comunitaria",
+      "Supervisión Profesional",
+      "Electivo I",
+      "Práctica Pre Profesional III",
+      "Seminario de Investigación I"
+    ]
+  },
+  {
+    nombre: "Ciclo 8",
+    cursos: [
+      "Psicología del Consumidor",
+      "Intervención Educativa",
+      "Psicología Escolar",
+      "Psicología Política",
+      "Gestión del Talento Humano",
+      "Electivo II",
+      "Práctica Pre Profesional IV",
+      "Seminario de Investigación II"
+    ]
+  },
+  {
+    nombre: "Ciclo 9",
+    cursos: [
+      "Evaluación de Proyectos Psicológicos",
+      "Psicología del Desarrollo Adulto y Vejez",
+      "Psicología Ambiental",
+      "Electivo III",
+      "Supervisión Clínica",
+      "Electivo IV",
+      "Práctica Pre Profesional V",
+      "Taller de Cierre Profesional"
+    ]
+  },
+  {
+    nombre: "Ciclo 10",
+    cursos: [
+      "Internado Psicológico I",
+      "Internado Psicológico II",
+      "Internado Psicológico III",
+      "Internado Psicológico IV",
+      "Internado Psicológico V",
+      "Internado Psicológico VI",
+      "Trabajo Final de Carrera I",
+      "Trabajo Final de Carrera II"
+    ]
   }
 ];
 
